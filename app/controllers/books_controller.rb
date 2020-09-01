4db1f69ec021816325df5de4706177beeec167ba
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-09-01 14:21:29 +0900

    [Finish] 応用課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,19 @@
 class BooksController < ApplicationController
-
+  before_action :authenticate_user!
   def show
-    @book = Book.find(params[:id])
+    @book = Book.new
+    @booking = Book.find(params[:id])
+    @user = current_user
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
@@ -19,30 +23,35 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
+    @booking = Book.find(params[:id])
+    if @booking.user == current_user
+      render 'edit'
+    else 
+      redirect_to books_path
+    end
   end
 
 
 
   def update
-    @book = Book.find(params[:id])
-    if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+    @booking = Book.find(params[:id])
+    if @booking.update(book_params)
+      redirect_to book_path(@booking), notice: "You have updated book successfully."
     else
       render "edit"
     end
   end
 
-  def delete
-    @book = Book.find(params[:id])
-    @book.destoy
+  def destroy
+    @booking = Book.find(params[:id])
+    @booking.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
   devise_for :users
   root 'home#top'
   get 'home/about'
+  get "users/:id" => "users#show", as: :mypage
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
 end
